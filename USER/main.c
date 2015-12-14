5b4096f9c2c2a5b0c93f65a980730c6f415dcd35
libertas <horizonvei@gmail.com>
2015-12-14 21:01:33 +0800

    merge printf

--- USER/main.c
+++ USER/main.c
@@ -5,52 +5,14 @@
 #include "clock.h"
 #include "usart.h"
 
-//test delay
-#if 0
-void tmp_config(void);
-
 int main(void)
 {
 	systick_config();
 	usart1_config(115200);
-	tmp_config();
-	
+
 	while(1)
 	{
-		GPIO_ResetBits(GPIOA,GPIO_Pin_12); 
-		delay_ms(1);
-		GPIO_SetBits(GPIOA,GPIO_Pin_12);
-		delay_ms(1);
 	}
 
 	return 0;
 }
-
-void tmp_config(void)
-{
-	GPIO_InitTypeDef GPIO_InitStructure;
-	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
-}
-#endif
-
-//test printf
-#if 1
-int main(void)
-{
-	systick_config();
-	usart1_config(115200);
-	
-	while(1){
-		printf("BUPT ROBOCON 2016\r\n");
-	}
-	return 0;
-}
-#endif
--- USER/usart.c
+++ USER/usart.c
@@ -73,52 +73,23 @@ void uprintf(USART_TypeDef* USARTx, char *fmt, ...)
 	
 	va_end(arg_ptr);
 }
-#if 0
-/*
-WARNING: NOT USABLE NOW!
-*/
-int fputc(int c, FILE *f)
-{
-	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-	
-	USART_SendData(USART1, (uint8_t) c);
-	
-	return c;
-}
-
-int fgetc(FILE *fp)
-{
-	int ch = 0;
-	while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
-
-	ch = (int)USART1->DR & 0xFF;
-
-	return ch;
-}
-#endif
-
-
-//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
-#if 1
-#pragma import(__use_no_semihosting)             
-//标准库需要的支持函数                 
+       
 struct __FILE 
 { 
 	int handle; 
-}; 
+};
 
-FILE __stdout;       
-//定义_sys_exit()以避免使用半主机模式    
-_sys_exit(int x) 
-{ 
-	x = x; 
-} 
+FILE __stdout;   
+ 
+int _sys_exit(int x)
+{
+	x = x;
+	return x;
+}
 
-//重定义fputc函数 
 int fputc(int ch, FILE *f)
-{ 	
-	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
-	USART1->DR = (uint8_t) ch;      
+{
+	while((USART1->SR&0X40)==0);
+	USART1->DR = (uint8_t) ch;
 	return ch;
 }
-#endif

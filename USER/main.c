53e3de055fddca621a33944d345c59bb80258e30
wanglumeng <wanglumeng@qq.com>
2015-12-22 23:13:48 +0800

    changed 84 to 168

--- USER/main.c
+++ USER/main.c
@@ -11,7 +11,7 @@ int main(void)
 	systick_config();
 	usart1_config(115200);
 	
-	TIM9_PWM_Init(500,84);		//84M/84=1Mhz的计数频率,重装载值500，所以PWM频率为 1M/500=2Khz.
+	TIM9_PWM_Init(500,168);		//84M/84=1Mhz的计数频率,重装载值500，所以PWM频率为 1M/500=2Khz.
 
 	TIM_SetCompare1(TIM9,200);//修改比较值，修改占空比
 	

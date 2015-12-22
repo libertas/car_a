add425fafe56f0b61d9316f0213b87d598152c05
wanglumeng <wanglumeng@qq.com>
2015-12-22 23:16:55 +0800

    zhushi pe6... only pe5 output one:

--- USER/hpwm.c
+++ USER/hpwm.c
@@ -23,7 +23,7 @@ void TIM9_PWM_Init(uint32_t arr,uint32_t psc)
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_Init(GPIOE,&GPIO_InitStructure);
-	
+#if 0
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
@@ -44,7 +44,7 @@ void TIM9_PWM_Init(uint32_t arr,uint32_t psc)
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
-	
+#endif
 	
 	TIM_TimeBaseStructure.TIM_Prescaler=psc - 1;  //定时器分频
 	TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up; //向上计数模式

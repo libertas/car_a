9b37015f3388e4dba99b930d7bcbddc2523c628c
libertas <horizonvei@gmail.com>
2016-01-01 21:58:58 +0800

    update

--- USER/three_wheel.c
+++ USER/three_wheel.c
@@ -47,8 +47,8 @@ void t_move_xy_c(int8_t spd_x, int8_t spd_y)
 {
 	int16_t arg_x, arg_y;
 	
-	arg_x = (int16_t) (((float) spd_x) / 256 * DEFAULT_ARG_SPEED);
-	arg_y = (int16_t) (((float) spd_y) / 256 * DEFAULT_ARG_SPEED);
+	arg_x = (int16_t) (((float) spd_x) / 128 * DEFAULT_ARG_SPEED);
+	arg_y = (int16_t) (((float) spd_y) / 128 * DEFAULT_ARG_SPEED);
 	
 	arg_speeds[0] = VECT_W0 * (0 + ABS(arg_y));
 	arg_speeds[1] = VECT_W1 * (ABS(arg_x) * 2 / sqrt(3) + ABS(arg_y) / cos(PI / 6));

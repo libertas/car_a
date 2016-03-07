b930465db98fd11099ac59c50a0545f5450d5363
libertas <horizonvei@gmail.com>
2016-03-07 18:57:49 +0800

    add basic instructions for auto_control

--- USER/auto_control.c
+++ USER/auto_control.c
@@ -1,12 +1,22 @@
 #include <stdint.h>
 
 #include "auto_control.h"
+#include "clock.h"
+#include "fan.h"
+#include "interpreter.h"
 
-void start(void)
+void auto_start(void)
 {
+	delay_ms(2000);
+	start_fan();
 }
 
-void (*auto_steps[])(void) = {start, 0};
+void auto_stop(void)
+{
+	check_cmd();
+}
+
+void (*auto_steps[])(void) = {auto_start, auto_stop, 0};
 
 void auto_control(void)
 {

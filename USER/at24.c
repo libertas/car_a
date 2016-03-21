60d70768c42eb7dfd2e87ee6ad53cfeb8f2d1e18
wanglumeng <wanglumeng@qq.com>
2016-03-21 20:26:26 +0800

    struct at GPIO(PB8.PB9) config

--- USER/at24.c
+++ USER/at24.c
@@ -15,12 +15,19 @@ void IIC_Init(void)
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);//使能GPIOB时钟
 
   //GPIOB8,B9初始化设置
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
-  GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化
+	GPIO_InitStructure.GPIO_Pin = IIC_SCL_GPIO.pin;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+	GPIO_Init(IIC_SCL_GPIO.port, &GPIO_InitStructure);//初始化
+	
+	GPIO_InitStructure.GPIO_Pin = IIC_SDA_GPIO.pin;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+	GPIO_Init(IIC_SDA_GPIO.port, &GPIO_InitStructure);//初始化
 	IIC_SCL=1;
 	IIC_SDA=1;
 }

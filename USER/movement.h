6aed39798816b77b906682cf023575252cbc3777
libertas <horizonvei@gmail.com>
2016-04-07 19:33:51 +0800

    change encoder's macro

--- USER/movement.h
+++ USER/movement.h
@@ -18,7 +18,7 @@ The world view:
 #define PI 3.141592653589793F
 
 #define W_DIAMETER ((float) 0.05)  // meter
-#define F_DIAMETER ((float) 0.028)  // meter
+#define F_DIAMETER ((float) 0.032)  // meter
 
 #define USE_FOUR_WHEEL
 

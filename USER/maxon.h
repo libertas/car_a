892252f6b88375c8b6554893befb1db3aa1b2cd4
c c <athersec@163.com>
2016-05-04 16:13:44 +0800

    maxonok

--- USER/maxon.h
+++ USER/maxon.h
@@ -1,3 +1,11 @@
+/*
+使用说明：
+1、调用maxon.h
+2、在maxon_config里修改maxon的配置；目前只配置了加速度和减速度以及控制模式
+3、maxon_up和maxon_down里分别为正转和反转
+4、maxon_stop目前是发速度为0的方式停止，不是disable
+*/
+
 void maxon_config(void);
 void maxon_up(int maxon_v);
 void maxon_down(int maxon_v);

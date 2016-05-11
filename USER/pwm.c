b9ecfb08ea7f7035593b3422bef7ac5343fa3fed
wanglumeng <wanglumeng@qq.com>
2016-05-11 22:21:48 +0800

    pwm #define CAR_A_1 / CAR_A_2

--- USER/pwm.c
+++ USER/pwm.c
@@ -4,6 +4,7 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_tim.h"
 
+#include "car.h"
 #include "pwm.h"
 
 #ifndef USE_HPWM
@@ -19,13 +20,31 @@ uint32_t PWMTotal[PWM_CHANNEL_NUM];
 	*无刷0.71stop,0.76/0.65正/反转
 	*舵机0~0.12
 */
+
+#ifdef CAR_A_1
 uint16_t PWMPins[PWM_CHANNEL_NUM] = {\
 	GPIO_Pin_6, GPIO_Pin_5, GPIO_Pin_9,\
 	GPIO_Pin_10, GPIO_Pin_8};
 GPIO_TypeDef *PWMPorts[PWM_CHANNEL_NUM] = {\
 	GPIOG, GPIOG, GPIOC,\
 	GPIOA, GPIOC};
+#endif
 
+#ifdef CAR_A_2
+/*
+	0 fan 涵道风扇
+	1 fan_updown 电机
+	2 fan_roll	not_using
+	3 mag_in/out 舵机	0.045(start) - 0.103(end)
+	4 mag_near/far not_using
+*/
+uint16_t PWMPins[PWM_CHANNEL_NUM] = {\
+	GPIO_Pin_6, GPIO_Pin_5, GPIO_Pin_9,\
+	GPIO_Pin_10, GPIO_Pin_8};
+GPIO_TypeDef *PWMPorts[PWM_CHANNEL_NUM] = {\
+	GPIOG, GPIOG, GPIOC,\
+	GPIOA, GPIOC};
+#endif
 
 void rcc_io_config(void)
 {
@@ -34,6 +53,62 @@ void rcc_io_config(void)
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
 }
 
+#ifdef CAR_A_1
+void pwm_config(void)
+{
+	float duties[PWM_CHANNEL_NUM] = {0.05, 0.075, 0.12, 0.045, 0.09};
+	unsigned long freqs[PWM_CHANNEL_NUM] = {50, 50, 50, 50, 50};
+	
+	uint8_t i;
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	
+	rcc_io_config();
+
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	for(i = 0; i < PWM_CHANNEL_NUM; i++) {
+		GPIO_InitStructure.GPIO_Pin = PWMPins[i];
+		GPIO_Init(PWMPorts[i], &GPIO_InitStructure);
+	}
+
+
+	for(i = 0; i < PWM_CHANNEL_NUM; i++) {
+		PWMTotal[i] = 10000;
+		PWMHighTime[i] = 5000;
+	}
+	
+	set_duties(duties);
+	set_freqs(freqs);
+
+
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+	
+	TIM_TimeBaseInitStructure.TIM_Period = 19;
+	TIM_TimeBaseInitStructure.TIM_Prescaler = PWM_PRESCALE;
+	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	
+	TIM_TimeBaseInit(TIM6, &TIM_TimeBaseInitStructure);
+	
+	TIM_ITConfig(TIM6, TIM_IT_Update, ENABLE);
+	TIM_Cmd(TIM6, ENABLE);
+	
+	NVIC_InitStructure.NVIC_IRQChannel = TIM6_DAC_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+#endif
+
+#ifdef CAR_A_2
 void pwm_config(void)
 {
 	float duties[PWM_CHANNEL_NUM] = {0.05, 0.075, 0.12, 0.045, 0.09};
@@ -102,7 +177,7 @@ void pwm_config(void)
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
 	GPIO_Init(GPIOE, &GPIO_InitStructure);
 }
-
+#endif
 
 void set_duty(uint8_t channel, float duty)
 {

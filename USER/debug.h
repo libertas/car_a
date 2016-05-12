36be4ebf2a4fce2e7125684c0e04150ad81d2730
c c <athersec@163.com>
2016-05-13 01:49:29 +0800

    nodbug

--- USER/debug.h
+++ USER/debug.h
@@ -4,9 +4,9 @@
 */
 
 
-#define DEBUG
-#define DEBUG_AUTO
-#define DEBUG_DB_EXEC
+//#define DEBUG
+//#define DEBUG_AUTO
+//#define DEBUG_DB_EXEC
 // #define DEBUG_SPI
 // #define DEBUG_POS_FAN
 // #define DEBUG_BRAKE

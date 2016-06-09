6973cae3e703b5102651141b39d18cc46ef7e117
wanglumeng <wanglumeng@qq.com>
2016-06-09 19:32:18 +0800

    reset 	modified:   USER/automove.h 	modified:   USER/push_rod.h 	new file:   USER/reset.c 	new file:   USER/reset.h

---
+++ USER/reset.c
@@ -0,0 +1,34 @@
+#include "automove.h"
+#include "clock.h"
+#include "fan.h"
+#include "push_rod.h"
+#include "switch.h"
+#include "whiteline.h"
+
+void reset1(void)//river
+{
+	gps_x = 0.2f;
+	gps_y = 6.7f;
+	gps_rad = 0;
+	push_rod_c(PUSH_ROD_PUSH, 0);
+	start_fan();
+	delay_ms(2000);
+	stop_fan();
+	wl_run();
+}
+
+void reset2(void)//N2
+{
+	gps_x = 0;
+	gps_y = 6.7f;
+	gps_rad = 0;
+	wl_run();
+}
+
+void reset3(void)//pole
+{
+	gps_x = 0.2f;
+	gps_y = 6.7f;
+	gps_rad = 0;
+	switch_nvic_enable(SWITCH_ENTER);
+}
